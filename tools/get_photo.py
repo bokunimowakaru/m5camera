2cb8e18799182ffb79c1c5ec5a182cf9920e3050
Wataru KUNINO <xbee@dream.jp>
2021-01-24 01:12:39 +0900

    改善

--- tools/get_photo.py
+++ tools/get_photo.py
@@ -9,11 +9,11 @@
 #                                          Copyright (c) 2016-2021 Wataru KUNINO
 ################################################################################
 
-DEVICE1='cam_a_5'                                   # 配信デバイス名(カメラ)
-DEVICE2='pir_s_5'                                   # 配信デバイス名(人感)
-SAVETO='photo'                                      # 保存先フォルダ名
-IP_CAM=''                                           # カメラのIPアドレス(仮)
-PORT=1024                                           # UDPポート番号を1024に
+DEVICE1 = 'cam_a_5'                                     # 配信デバイス名(カメラ)
+DEVICE2 = 'pir_s_5'                                     # 配信デバイス名(人感)
+SAVETO  = 'photo'                                       # 保存先フォルダ名
+IP_CAM  = None                                          # カメラのIPアドレス
+PORT    = 1024                                          # UDPポート番号を1024に
 
 import pathlib                                          # ファイル・パス用
 import socket                                           # ソケット通信用
@@ -28,11 +28,10 @@ def cam(ip, filename = 'cam.jpg'):                      # IoTカメラ
     try:
         res = urllib.request.urlopen(url_s + s)         # IoTカメラで撮影を実行
         if res.headers['content-type'].lower().find('image/jpeg') < 0:
-            res = None                                  # JPEGで無いときにNone
+            print('Error content-type :',res.headers['content-type'])
+            return None                                 # JPEGで無いときにNone
     except urllib.error.URLError:                       # 例外処理発生時
-        res = None                                      # エラー時にNoneを代入
-    if res is None:                                     # resがNoneのとき
-        print('Error Camera :',url_s)                   # エラー表示
+        print('Error urllib :',url_s)                   # エラー表示
         return None                                     # 関数を終了する
     data = res.read()                                   # コンテンツ(JPEG)を読む
     try:
@@ -42,12 +41,12 @@ def cam(ip, filename = 'cam.jpg'):                      # IoTカメラ
         return None                                     # 関数を終了する
     fp.write(data)                                      # 写真ファイルを保存する
     fp.close()                                          # ファイルを閉じる
-    print('filename =',filename)                        # 保存ファイルを表示する
+    print('saved file :',filename)                      # 保存ファイルを表示する
     return filename                                     # ファイル名を応答する
 
 print('Get Photo for Python')                           # タイトル表示
 pathlib.Path(SAVETO).mkdir(exist_ok=True)               # フォルダ作成
-time_start = time.time()                                # 開始時刻シリアル値を保持
+time_start = time.time()                                # 開始時刻シリアル値保持
 
 print('Listening UDP port', PORT, '...')                # ポート番号表示
 try:
@@ -58,9 +57,9 @@ except Exception as e:                                  # 例外処理発生時
     print(e)                                            # エラー内容を表示
     exit()                                              # プログラムの終了
 while sock:                                             # 永遠に繰り返す
-    udp, udp_from = sock.recvfrom(128)                  # UDPパケットを取得
     try:
-        udp = udp.decode()                              # UDPデータを文字列に変換
+        udp, udp_from = sock.recvfrom(128)              # UDPパケットを取得
+        udp = udp.decode()                              # UDPデータを文字列に
     except Exception as e:                              # 例外処理発生時
         print(e)                                        # エラー内容を表示
         continue                                        # whileの先頭に戻る
@@ -68,13 +67,16 @@ while sock:                                             # 永遠に繰り返す
     for c in udp:                                       # UDPパケット内
         if ord(c) > ord(' ') and ord(c) <= ord('~'):    # 表示可能文字
             s += c                                      # 文字列sへ追加
-    date=datetime.datetime.today()                      # 日付を取得
-    print(date.strftime('%Y/%m/%d %H:%M')+', ', end='') # 日付を出力
+    date = datetime.datetime.today()                    # 日時を取得
+    date_s = date.strftime('%Y/%m/%d %H:%M:%S')         # 日時を文字列に変換
+    print(date_s + ', ', end='')                        # 日時を出力
     print(udp_from[0], end='')                          # 送信元アドレスを出力
     print(', ' + s, flush=True)                         # 受信データを出力
+    if s[5] != '_' or s[7] != ',':                      # 指定書式で無い時
+        continue                                        # whileの先頭に戻る
     device = s[0:7]                                     # 先頭7文字をデバイス名
     value = s.split(',')                                # CSVデータを分割
-    if (device == DEVICE1):                             # カメラから受信
+    if device == DEVICE1:                               # カメラから受信
         if (time.time() - time_start < 300):            # 起動後5分以内
             IP_CAM = udp_from[0]                        # カメラIPアドレスを保持
             print('IP_CAM =',IP_CAM)                    # 表示
@@ -82,8 +84,7 @@ while sock:                                             # 永遠に繰り返す
             print('起動後5分を経過したので送信先は更新しません')
             continue                                    # whileの先頭に戻る
     if device == DEVICE1 or device == DEVICE2:          # 対象機器の時
-        if IP_CAM != '':                                # IPアドレス保持時
-            date_s = date.strftime('%Y%m%d-%H%M%S')     # ファイル名用の時刻書式
-            filename = 'cam_' + date_s + '.jpg'         # ファイル名を作成
-            cam(IP_CAM,filename)                        # 撮影(ファイル保存)
+        if IP_CAM is not None:                          # IP_CAMがNoneでは無い時
+            date_f = date.strftime('%Y%m%d-%H%M%S')     # ファイル名用の時刻書式
+            cam(IP_CAM,'cam_' + date_f + '.jpg')        # 撮影(ファイル保存)
 sock.close()                                            # ソケットの切断
